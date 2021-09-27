be24eff5c9919b4aa1760e27f545b3e8d253a649
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-09-27 22:31:45 +0200

    finish todo

--- src/component/Form/Form.tsx
+++ src/component/Form/Form.tsx
@@ -1,33 +1,53 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ITodo } from "../../interfaces/ITodo";
 import Item from "../Item/Item";
 import { v4 as uuidv4 } from "uuid";
 
-export default function Form(): JSX.Element {
-  const [itemValue, setItemValue] = useState<ITodo[]>([
+export default function Form() {
+  const [itemArr, setItemArr] = useState<ITodo[]>([
     { id: uuidv4(), value: "Allumer le pc" },
     { id: uuidv4(), value: "Ouvrir le terminal" },
     { id: uuidv4(), value: "Executer un yarn start" },
   ]);
+  const [stateInput, setStateInput] = useState<ITodo | string>();
   const deleteTask = (idTask: string): void => {
-    const dataState = itemValue.filter((item) => {
+    const dataState = itemArr.filter((item) => {
       return item.id !== idTask;
     });
-    setItemValue(dataState);
+    setItemArr(dataState);
+  };
+  const addTodo = (): void => {
+    const newitemArr = [...itemArr];
+    const newTodo: any = {};
+    newTodo.value = stateInput;
+    newTodo.id = uuidv4();
+    newitemArr.push(newTodo);
+    setItemArr(newitemArr);
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setStateInput(e.target.value);
+  };
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
-      <form className="mb-3">
+      <div className="mb-3">
         <label className="form-label mt-3" htmlFor="todo">
           Chose à faire
         </label>
-        <input className="form-control" id="#todo" type="text" />
-        <button className="mt-2 btn btn-primary d-block">Envoyer</button>
-      </form>
+        <input
+          className="form-control mb-2"
+          id="#todo"
+          type="text"
+          onChange={handleChange}
+        />
+        <button className=" btn btn-primary d-block" onClick={addTodo}>
+          Envoyer
+        </button>
+      </div>
+
       <h2>Liste des choses à faire :</h2>
       <ul className="list-group">
-        {itemValue?.map((item) => {
+        {itemArr?.map((item) => {
           return (
             <Item
               value={item.value}
--- src/component/Item/Item.tsx
+++ src/component/Item/Item.tsx
@@ -1,6 +1,6 @@
 import { ITodo } from "../../interfaces/ITodo";
 
-export default function Item(props: ITodo): JSX.Element {
+export default function Item(props: ITodo) {
   return (
     <div>
       <li className="border d-flex justify-content-between align-items-center p-2 m-2">
